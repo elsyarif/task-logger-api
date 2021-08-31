9aa0516547a0656120f8071ea90394774e94eb73
elSyarif <id.syarif@gmail.com>
2021-09-01 06:29:24 +0700

    setup index

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import http from "http";
+import * as socket from "socket.io";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+const server = http.createServer(app);
+const io = new socket.Server(server, {
+  transports: ["websocket"],
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("server connected");
+
+  socket.on("disconnect", (soc) => {
+    console.log("client disconnected");
+  });
+});
+
+export { io };
+server.listen(PORT, () => {
+  console.log(`server running on port : ${PORT}`);
+});
